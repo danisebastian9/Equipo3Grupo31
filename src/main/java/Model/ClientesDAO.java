4484afce5362c4c251e852d6b5914f2c9ba114eb
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-09-22 15:07:38 -0500

    DAO added

--- src/main/java/Model/ClientesDAO.java
+++ src/main/java/Model/ClientesDAO.java
@@ -3,6 +3,9 @@ package Model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 import Controller.Conexion;
 
@@ -13,5 +16,20 @@ public class ClientesDAO {
 	PreparedStatement ps = null;
 	ResultSet res = null;
 	
-	public boolean Agregar_Cliente()
+	public boolean Agregar_Cliente(ClientesDTO cliente) {
+		boolean result = false;
+		
+		try {
+			String sql="INSERT INTO clientes VALUES(?,?,?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, cliente.getCedula());
+			ps.setString(2, cliente.getDireccion());
+			ps.setString(3, cliente.getEmail());
+			ps.setString(4, cliente.getNombre());
+			ps.setString(5, cliente.getTelefono());
+		}catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error al insertar: " +ex);
+		}
+		return result;
+	}
 }
