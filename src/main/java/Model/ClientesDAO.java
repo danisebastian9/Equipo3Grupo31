542866b22b514ed40eb4aa6de7c763239562963f
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-09-22 14:52:13 -0500

    Clientes DTO added

--- src/main/java/Model/ClientesDAO.java
+++ src/main/java/Model/ClientesDAO.java
@@ -1,5 +1,17 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import Controller.Conexion;
+
 public class ClientesDAO {
 
+	Conexion cnn = new Conexion();
+	Connection con = cnn.Conectar();
+	PreparedStatement ps = null;
+	ResultSet res = null;
+	
+	public boolean Agregar_Cliente()
 }
--- src/main/java/Model/ClientesDTO.java
+++ src/main/java/Model/ClientesDTO.java
@@ -2,4 +2,61 @@ package Model;
 
 public class ClientesDTO {
 
+	private String direccion;
+	private String email;
+	private String nombre;
+	private String telefono;
+	private int cedula;
+	
+	public ClientesDTO(String direccion, String email, String nombre, String telefono, int cedula) {
+		super();
+		this.direccion = direccion;
+		this.email = email;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.cedula = cedula;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	
+	
 }
+
